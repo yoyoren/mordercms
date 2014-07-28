87a3d315f0d8a6e42118b068a74534d8a10366a9
姚丽梅 <yaolimei@xiaomi.com>
2014-07-28 18:58:36 +0800

    change db config

--- includes/config.php
+++ includes/config.php
@@ -2,15 +2,15 @@
 
 //写操作服务器配置
 $my_db_host_write   = "210.51.166.149";
-$my_db_name_write   = "root";
-$my_db_user_write   = "mescake";
+$my_db_name_write   = "mescake";
+$my_db_user_write   = "root";
 $my_db_pass_write   = "";
 
 
 //读操作服务器配置
 $my_db_host_read   = "210.51.166.149";
-$my_db_name_read   = "root";
-$my_db_user_read   = "mescake";
+$my_db_name_read   = "mescake";
+$my_db_user_read   = "root";
 $my_db_pass_read   = "";
 
 
